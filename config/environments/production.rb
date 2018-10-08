d6a4333dd244afa0d8209772811e5decd164fc31
doctorlam <chris.lam.unt@gmail.com>
2018-10-08 13:21:03 -0500

    moved config stuff insdie of end

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
-config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::SessionsController.force_ssl }
 config.to_prepare { Devise::RegistrationsController.force_ssl }
 config.to_prepare { Devise::PasswordsController.force_ssl }
 end
